51768fcfdd203a562e16012959c557525c33a8a9
Luke Kanies <luke@madstop.com>
2007-08-17 17:25:46 +0200

    Converting from Mercurial to Git

---
+++ manifests/config.pp
@@ -0,0 +1,12 @@
+define nginx::config($basedir, $servers, $port_number) {
+    file { "/etc/nginx/nginx.conf":
+        content => template("nginx/nginx.conf.erb"),
+        mode => 644,
+        owner => root,
+        notify => Service[nginx]
+    }
+
+    realize Service[nginx]
+}
+
+# $Id$
---
+++ manifests/init.pp
@@ -0,0 +1,12 @@
+class nginx {
+	package { nginx: ensure => installed }
+
+    # Create the server as a virtual resource, so config instances
+    # can enable it.
+    @service { nginx:
+        ensure => running,
+        enable => true
+    }
+}
+
+# $Id$
